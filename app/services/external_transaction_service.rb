2b2ea1a6e6763198707443ffc0973ca327801bbb
Nirankar Singh <nirankarsingh247@gmail.com>
2025-04-24 15:09:59 +0530

    update api details

--- app/services/external_transaction_service.rb
+++ app/services/external_transaction_service.rb
@@ -3,21 +3,28 @@ class ExternalTransactionService
   base_uri 'points-processor.free.beeceptor.com'
 
   def self.process_transaction(transaction)
-    response = post("/transactions", body: { transaction: transaction })
+    response = post("/transactions", body: { transaction_id: transaction[:tranasction_id] })
     status = response.success? ? :success : :failed
     transaction[:status]= status
+    transaction[:amount] = response["amount"]
+    transaction[:transaction_reference] = response["transaction_reference"]
 
     transaction
   end
 
   def self.bulk_process(transactions)
-    response = post("/transactions/bulk_process", body: { transactions: transactions })
+    response = post("/transactions/bulk_process", body: { transaction_ids: transactions.pluck(:transaction_id) })
     return [] unless response.success?
 
     trans = []
     transactions.each do |transaction|
       tr = response.detect { |t| t["transaction_id"] == transaction[:transaction_id] }
-      transaction.merge!(status: tr.present? ? :success : :failed)
+      if tr.present?
+        transaction.merge!(amount: tr["amount"], transaction_reference: tr["transaction_reference"], status: :success)
+      else
+        transaction.merge!(amount: 0, transaction_reference: nil, status: :falied)
+      end
+
       trans << transaction
     end
 
--- spec/services/external_transaction_service_spec.rb
+++ spec/services/external_transaction_service_spec.rb
@@ -3,26 +3,36 @@ require 'rails_helper'
 RSpec.describe ExternalTransactionService do
   describe ".process_transaction" do
     it "returns parsed transaction data" do
-      transaction = { transaction_id: 'abc123', points: 50, user_id: 'user_1' }
+      transaction = { transaction_id: 'abc123', amount: 50, transaction_reference: 'user_1' }
       stub_request(:post, "http://points-processor.free.beeceptor.com/transactions")
         .to_return(body: transaction.to_json, headers: { 'Content-Type' => 'application/json' })
 
-      result = ExternalTransactionService.process_transaction(transaction)
-
-      expect(result).to eq({
+      result = ExternalTransactionService.process_transaction({
         transaction_id: 'abc123',
         points: 50,
         user_id: 'user_1',
+      })
+
+      expect(result).to eq({
+        transaction_id: 'abc123',
+        amount: 50,
+        points: 50,
+        transaction_reference: 'user_1',
+        user_id: "user_1",
         status: :success
       })
     end
 
     it "returns error parsed transaction data" do
-      transaction = { transaction_id: 'abc123', points: 50, user_id: 'user_1' }
+      transaction = { transaction_id: 'abc123', points: 50, amount: nil,  user_id: 'user_1', transaction_reference: nil }
       stub_request(:post, "http://points-processor.free.beeceptor.com/transactions")
         .to_return(body: '', headers: { 'Content-Type' => 'application/json' }, status: :bad_request)
 
-      result = ExternalTransactionService.process_transaction(transaction)
+      result = ExternalTransactionService.process_transaction({
+        transaction_id: 'abc123',
+        points: 50,
+        user_id: 'user_1',
+      })
       transaction[:status] =:failed
 
       expect(result).to eq(transaction)
@@ -34,8 +44,8 @@ RSpec.describe ExternalTransactionService do
       stub_request(:post, "http://points-processor.free.beeceptor.com/transactions/bulk_process")
         .to_return(body: [{
           transaction_id: 'abc123',
-          points: 50,
-          user_id: 'user_1',
+          amount: 50,
+          transaction_reference: 'user_1',
         }].to_json, headers: { 'Content-Type' => 'application/json' })
 
       result = ExternalTransactionService.bulk_process([{
@@ -44,7 +54,7 @@ RSpec.describe ExternalTransactionService do
           user_id: 'user_1',
         }])
 
-      expect(result).to eq([{points: 50, status: :success, transaction_id: "abc123", user_id: "user_1"}])
+      expect(result).to eq([{ amount: 50, points: 50, status: :success, transaction_id: "abc123", transaction_reference: "user_1", user_id: "user_1"}])
     end
 
     it "returns error parsed transaction data" do
